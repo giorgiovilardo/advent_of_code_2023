61a0bb8c75abc790be3a291d17854a4befa29f48
Giorgio Vilardo <giorgio@vilardo.eu>
2023-12-01 14:54:43 +0100

    feat: done day 1 part 2

--- lib/day1.rb
+++ lib/day1.rb
@@ -3,9 +3,29 @@
 require_relative "helper/aoc_input"
 
 module Day1
+  MAPPINGS = {
+    "one" => "o1e",
+    "two" => "t2o",
+    "three" => "t3e",
+    "four" => "f4r",
+    "five" => "f5e",
+    "six" => "s6x",
+    "seven" => "s7n",
+    "eight" => "e8t",
+    "nine" => "n9e",
+  }.freeze
+
   def self.part_one(input = AocInput.read_day(1))
     input.map { |line| line.delete("^0-9") }
          .map { |number_string| (number_string[0] + number_string[-1]).to_i }
          .sum
+  end
+
+  def self.part_two(input = AocInput.read_day(1))
+    part_one(
+      input.map do |line|
+        MAPPINGS.inject(line) { |temp_str, (key, value)| temp_str.gsub(key, value) }
+      end
+    )
   end
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -4,7 +4,7 @@ require "rspec"
 require_relative "../lib/day1"
 
 RSpec.describe Day1 do
-  describe "test data of part 1" do
+  describe "with test data of part 1" do
     it "should return 142" do
       input = "1abc2
 pqr3stu8vwx
@@ -19,6 +19,27 @@ treb7uchet".split("\n")
     it "should return the correct value" do
       result = Day1.part_one
       expect(result).to eq(55971)
+    end
+  end
+
+  describe "with test data of part 2" do
+    it "should return 281" do
+      input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen".split("\n")
+      result = Day1.part_two(input)
+      expect(result).to eq(281)
+    end
+  end
+
+  describe "with real data of part 2" do
+    it "should return the correct value" do
+      result = Day1.part_two
+      expect(result).to eq(54719)
     end
   end
 end
