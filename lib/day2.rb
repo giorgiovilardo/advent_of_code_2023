db68c3fb50e6d18eff76315bf128bbf173a9ef6f
Giorgio Vilardo <giorgio@vilardo.eu>
2023-12-02 15:55:59 +0100

    feat: done day 2 part 1

--- lib/day2.rb
+++ lib/day2.rb
@@ -4,16 +4,54 @@ require_relative "helper/aoc_input"
 
 module Day2
   MAX_CUBES = {
-    "red" => 12,
-    "green" => 13,
-    "blue" => 14,
+    red: 12,
+    green: 13,
+    blue: 14,
   }
 
   def self.part_one(input = AocInput.read_day(2))
-    1
+    parsed_games = input.map { |line| parse_gameline(line) }
+      .map do |results|
+      { id: results[:id] }.merge(
+        results[:games].reduce do |memo, hash|
+          hash.merge(memo) { |_, oldval, newval| [oldval, newval].max }
+        end
+      )
+    end
+
+    parsed_games.select do |item|
+      [:red, :green, :blue].all? { |color| item[color] <= MAX_CUBES[color] }
+    end.map { |item| item[:id] }.sum
   end
 
   def self.part_two(input = AocInput.read_day(2))
     1
+  end
+
+  # @param line [String]
+  def self.parse_gameline(line)
+    # just one comma guaranteed, safe to split and destructure
+    game_id, game_results = line.split(":")
+
+    game_id = game_id[/\d+/].to_i
+    results = game_results.split(";").map do
+      |game_result|
+      parse_result(game_result.strip.split(","))
+    end
+
+    {
+      id: game_id,
+      games: results,
+    }
+  end
+
+  # @param results [Array<String>]
+  # @return [Hash{Symbol->Integer}]
+  def self.parse_result(results)
+    color_values = results.map { |result|
+      id, color = result.split
+      [color.to_sym, id.to_i]
+    }.to_h
+    { red: 0, green: 0, blue: 0 }.merge(color_values)
   end
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -5,7 +5,7 @@ require_relative "../lib/day2"
 
 RSpec.describe Day2 do
   describe "with test data of part 1" do
-    it "should return 142" do
+    it "should return 8" do
       input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
@@ -18,24 +18,20 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".split("\n")
 
   describe "with real data of part 1" do
     it "should return the correct value" do
-      pending
       result = Day2.part_one
-      expect(result).to eq(55971)
+      expect(result).to eq(2369)
     end
   end
 
   describe "with test data of part 2" do
-    it "should return 281" do
-      pending
-      input = "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen".split("\n")
+    it "should return 2286" do
+      input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".split("\n")
       result = Day2.part_two(input)
-      expect(result).to eq(281)
+      expect(result).to eq(2286)
     end
   end
 
